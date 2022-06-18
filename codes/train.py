2d9776ba25287ba7fba7a17bcc030bfe348911ba
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2022-06-18 14:10:58 +0530

    Update train.py

--- codes/train.py
+++ codes/train.py
@@ -228,7 +228,7 @@ def main():
 #                 logger.info('# Validation # PSNR: {:.4e}.'.format(avg_psnr))
 #                 logger_val = logging.getLogger('val')  # validation logger
 #                 logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}.'.format(
-                    epoch, current_step, avg_psnr))
+#                     epoch, current_step, avg_psnr))
                 # tensorboard logger
 #                 if opt['use_tb_logger'] and 'debug' not in opt['name']:
 #                     tb_logger.add_scalar('psnr', avg_psnr, current_step)
